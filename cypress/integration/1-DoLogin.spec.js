a194034ae31fac24a93b54721c5a599bf800bbb2
SimoneFarias <simone.farias@geru.com.br>
2021-07-19 00:32:59 -0300

    update tests

---
+++ cypress/integration/1-DoLogin.spec.js
@@ -0,0 +1,18 @@
+const { describe } = require("mocha");
+
+describe("Do Login", () => {
+    beforeEach(() => cy.visit("https://dev.deel.wtf/login"));
+   
+    it("Login", () => {
+      
+        const login = "silvafar@gmail.com"        
+        const password = "Simone@Helo01" 
+       
+        cy.xpath('//input[@name="email"]').type(login);
+        cy.xpath('//input[@name="password"]').type(password);
+        cy.xpath('//button[@type="submit"]').click();
+        cy.xpath('//button[@data-qa="close"]').should('be.visible').click()
+        cy.xpath('//h1[@data-qa="heading"]').should('be.visible');
+    })  
+});
+
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,11 +4,3 @@ const fetch = require('node-fetch')
 module.exports = (on, config) => {
   
 }
-
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-// import * as allureWriter from "@shelex/cypress-allure-plugin/writer";
-
-module.exports = (on, config) => {
-    allureWriter(on, config);
-    return config;
-};
